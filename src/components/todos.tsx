71b0a9f5f4522b4484158dbbd11b675c240aecb8
jihanv <hedjmny@gmail.com>
2025-10-19 16:45:58 +0900

    Add form in Todos component that uses useCreateTodo mutation, so we can edit the json from the client sent data.

--- src/components/todos.tsx
+++ src/components/todos.tsx
@@ -1,5 +1,8 @@
 // import { useIsFetching } from '@tanstack/react-query';
+import { useForm, type SubmitHandler } from 'react-hook-form';
+import { useCreateTodo } from '../services/mutations';
 import { useTodos, useTodosIds } from '../services/queries'
+import type { Todo } from '../types/todo';
 
 export default function Todos() {
 
@@ -8,14 +11,39 @@ export default function Todos() {
     const todosIdsQuery = useTodosIds();
     const todosQueries = useTodos(todosIdsQuery.data)
 
+    const createTodoMutation = useCreateTodo()
+
+    const handleCreateToDoSubmit: SubmitHandler<Todo> = (data) => {
+        createTodoMutation.mutate(data)
+    }
+    // Initialize React Hook Form and tell it the shape of our form data is Todo
+    // useForm returns many helpers, but we only need register and handleSubmit right now
+    // register() connects an input field to the form state
+    // handleSubmit() wraps our submit function and gives us validated form data
+    //<Todo> ensures form fields match the Todo type for type safety
+    const { register, handleSubmit } = useForm<Todo>()
+
+
+
     if (todosIdsQuery.isPending) {
         return <span>Loading...</span>;
     }
 
     return (
         <>
+            <form
+                // React Hook Form wraps submission using handleSubmit
+                // It prevents the page from refreshing and gives us form values safely
+                onSubmit={handleSubmit(handleCreateToDoSubmit)}>
+                <input placeholder='Title' {...register("title")}></input>
+                <br />
+                <input placeholder='Description' {...register("description")}></input>
+                <br />
+                <input type="submit"></input>
+
+            </form>
             <p>Query function status: {todosIdsQuery.fetchStatus}</p>
-            <div>{todosIdsQuery.data?.map((id) => <p key={id}>id: {id}</p>)}</div>
+
             <ul>
                 {todosQueries.map(({ data }, i) => (
                     <li key={i}>
--- src/services/mutations.ts
+++ src/services/mutations.ts
@@ -5,5 +5,21 @@ import { createTodo } from "./api";
 export function useCreateTodo() {
   return useMutation({
     mutationFn: (data: Todo) => createTodo(data),
+
+    onMutate: () => {
+      console.log("mutate");
+    },
+
+    onError: () => {
+      console.log("error");
+    },
+
+    onSuccess: () => {
+      console.log("success");
+    },
+
+    onSettled: () => {
+      console.log("settled");
+    },
   });
 }
